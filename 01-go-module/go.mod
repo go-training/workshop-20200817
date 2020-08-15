83684eb0e357fbe359a74fd5b71d2e042c335734
Bo-Yi Wu <appleboy.tw@gmail.com>
2020-08-15 12:28:37 +0800

    chore(01): update

--- 01-go-module/go.mod
+++ 01-go-module/go.mod
@@ -1,3 +1,5 @@
 module hello
 
 go 1.14
+
+require github.com/go-training/workshop-20200817 v0.0.0-20200815042659-67bb8cbd042b
--- 01-go-module/main.go
+++ 01-go-module/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	hello "github.com/go-training/workshop-20200817/00-hello-module"
+)
 
 func main() {
 	fmt.Println("hello world")
+	fmt.Println(hello.World())
 }
