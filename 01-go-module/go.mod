a2a8732afdedf1704fecac0558655fb0f218945f
Bo-Yi Wu <appleboy.tw@gmail.com>
2020-08-15 14:20:05 +0800

    chore(01): add godotenv

--- 01-go-module/go.mod
+++ 01-go-module/go.mod
@@ -2,4 +2,7 @@ module hello
 
 go 1.14
 
-require github.com/go-training/workshop-20200817 v0.0.0-20200815042659-67bb8cbd042b
+require (
+	github.com/go-training/workshop-20200817 v0.0.0-20200815042659-67bb8cbd042b
+	github.com/joho/godotenv v1.3.0 // indirect
+)
--- 01-go-module/main.go
+++ 01-go-module/main.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	hello "github.com/go-training/workshop-20200817/00-hello-module"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	fmt.Println("hello world")
 	fmt.Println(hello.World())
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	s3Bucket := os.Getenv("S3_BUCKET")
+	fmt.Println(s3Bucket)
 }
