b66f931e2b285a9d0925a16348d2e7d01b6b89f7
Bo-Yi Wu <appleboy.tw@gmail.com>
2020-08-16 14:27:07 +0800

    chore(07): support channel

--- 07-channel/main.go
+++ 07-channel/main.go
@@ -6,13 +6,46 @@ import (
 )
 
 func main() {
-	go foobar("foobar")
-	time.Sleep(2 * time.Second)
+	example01()
+	example02()
+	example03()
 }
 
-func foobar(v string) {
-	for i := 1; i <= 5; i++ {
+// buffer vs unbuffer channel
+func example03() {
+	c := make(chan bool)
+	go func() {
+		fmt.Println("send to channel")
+		c <- true
+	}()
+	<-c
+	fmt.Println("Done")
+}
+
+func example01() {
+	c := make(chan string)
+	go foobar("example01", c)
+	for {
+		v, ok := <-c
+		if !ok {
+			break
+		}
 		fmt.Println(v)
+	}
+}
+
+func example02() {
+	c := make(chan string)
+	go foobar("example02", c)
+	for v := range c {
+		fmt.Println(v)
+	}
+}
+
+func foobar(v string, c chan string) {
+	for i := 1; i <= 5; i++ {
+		c <- v
 		time.Sleep(500 * time.Millisecond)
 	}
+	close(c)
 }
