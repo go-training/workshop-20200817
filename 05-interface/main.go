a6d8b94b50320f68c6c6246bdfc94d3a3d3ad9e0
Bo-Yi Wu <appleboy.tw@gmail.com>
2020-08-16 09:45:20 +0800

    chore(05): add type assertion

--- 05-interface/main.go
+++ 05-interface/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+
 	"test/bmw"
 	"test/toyota"
 )
@@ -35,10 +37,31 @@ func main() {
 	for s, v := range ips {
 		fmt.Printf("%v, %v\n", s, v)
 	}
+
+	printF(1)
+	printF("1")
 }
 
 type ipv4 []byte
 
 func (s ipv4) String() string {
 	return fmt.Sprintf("%v.%v.%v.%v", s[0], s[1], s[2], s[3])
+}
+
+// type assertion
+func printF(val interface{}) error {
+	if v, ok := val.(string); ok {
+		fmt.Println("this is string via if:", v)
+	} else if v, ok := val.(int); ok {
+		fmt.Println("this is int via if:", v)
+	}
+
+	switch v := val.(type) {
+	case string:
+		fmt.Println("this is string via switch:", v)
+	case int:
+		fmt.Println("this is int via switch:", v)
+	}
+
+	return errors.New("Error: this is not string")
 }
